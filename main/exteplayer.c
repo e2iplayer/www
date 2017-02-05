d3c263ba38869f41787ef9fae3b2ac291829a744
samsamsam <samsamsam@o2.pl>
2017-02-05 16:49:02 +0100

    Deploy new version 31

--- main/exteplayer.c
+++ main/exteplayer.c
@@ -400,7 +400,7 @@ int main(int argc, char* argv[])
     memset(argvBuff, '\0', sizeof(argvBuff));
     int commandRetVal = -1;
     /* inform client that we can handle additional commands */
-    fprintf(stderr, "{\"EPLAYER3_EXTENDED\":{\"version\":%d}}\n", 30);
+    fprintf(stderr, "{\"EPLAYER3_EXTENDED\":{\"version\":%d}}\n", 31);
 
     if (0 != ParseParams(argc, argv, file, audioFile, &audioTrackIdx, &subtitleTrackIdx))
     {
