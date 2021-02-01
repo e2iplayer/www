728c8987e0759d5ee50a9b965b5e99af5dffb2a9
samsamsam <samsamsam@o2.pl>
2021-02-01 18:15:16 +0100

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# exteplayer3 - project has been closed - please start to use different branch
-Core of movie player for E2 based on the libeplayer using the ffmpeg solution
--- external/plugins/png.h
+++
@@ -1,8 +0,0 @@
-#ifndef _exteplayer3_external_plugins_png_
-#define _exteplayer3_external_plugins_png_
-
-int PNGPlugin_saveRGBAImage(const char *filename, const unsigned char *data, int width, int height);
-int PNGPlugin_init(void);
-int PNGPlugin_term(void);
-
-#endif // _exteplayer3_external_plugins_png_
